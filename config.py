13b0fed87f37370d73b191df90e440e43b283171
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2021-10-07 23:04:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", 
-"
-╭━━╮╱╱╭╮╱╱╭━╮
+START_MSG = os.environ.get("START_MESSAGE", "╭━━╮╱╱╭╮╱╱╭━╮
 ┃╭╮┣━╮┃╰┳┳┫━╋━┳┳╮
 ┃╭╮┃╋╰┫╋┃┃┣━┃┻╋┃┫
 ╰━━┻━━┻━╋╮┣━┻━┻┻╯
